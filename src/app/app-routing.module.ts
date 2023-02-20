a42ccbc90ecfe3c63f613ee17b04e993d80d83d6
HouseCham <ramsesramirezvallejo@gmail.com>
2023-02-20 16:35:39 -0600

    yep

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'cliente', component: ClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
